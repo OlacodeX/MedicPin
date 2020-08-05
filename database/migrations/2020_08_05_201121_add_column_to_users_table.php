6bd94e5d8c49c1202e59ab504220e6890e4bf5c9
olacodeX <aluko798@gmail.com>
2020-08-05 23:14:56 +0100

    two

--- database/migrations/2020_08_05_201121_add_column_to_users_table.php
+++ database/migrations/2020_08_05_201121_add_column_to_users_table.php
@@ -15,7 +15,7 @@ class AddColumnToUsersTable extends Migration
     {
         Schema::table('users', function (Blueprint $table) {
             //
-            $table->string('role');
+            $table->string('role')->nullable();
         });
     }
 
